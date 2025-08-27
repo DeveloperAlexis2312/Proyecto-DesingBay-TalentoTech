377e77b88385d5802adfa8b1702dfc227cf88825
katherinperez <katherinpg2311@gmail.com>
2025-08-26 22:30:10 -0500

    Se acomodaron las carpetas

--- script.js
+++
@@ -1,8 +0,0 @@
-function openMenu() {
-  document.getElementById("mySidebar").style.width = "250px";
-}
-
-// Cierra el menú
-function closeMenu() {
-  document.getElementById("mySidebar").style.width = "0";
-}
