d9b24e7d9165852313beae3df5da3ee2b2bdd3a2
Ruben Alexis Morales <rubenalexis2312@gmail.com>
2025-08-27 22:09:41 -0500

    Reorganizacion y correccion de errores

---
+++ FRONTEND/js/index.js
@@ -0,0 +1,7 @@
+function openMenu() {                             // Abre el menú
+  document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeMenu() {                            // Cierra el menú
+  document.getElementById("mySidebar").style.width = "0";
+}
